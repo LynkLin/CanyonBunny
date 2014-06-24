e189e04bf6426f327e2e507a5ac38efd7015b7bf
LynkLin@gmail.com <LynkLin@gmail.com>
2014-06-24 16:28:35 +0800

    Initial commit

---
+++ core/src/com/lynk/game/canyonbunny/util/Constants.java
@@ -0,0 +1,12 @@
+package com.lynk.game.canyonbunny.util;
+
+/**
+ * Created by Lynk on 14-6-24.
+ */
+public class Constants {
+    //Visible game world is 5 meters wide
+    public static final float VIEWPORT_WIDTH = 0.5F;
+
+    //Visible game world is 5 meters tall
+    public static final float VIEWPORT_HEIGHT = 0.5F;
+}
