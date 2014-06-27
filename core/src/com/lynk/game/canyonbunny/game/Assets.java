f5fb39c7c80d71cc40af4c763ec94a83947bdcec
LynkLin@gmail.com <LynkLin@gmail.com>
2014-06-27 16:43:31 +0800

    Update 2014-06-27

---
+++ core/src/com/lynk/game/canyonbunny/game/Assets.java
@@ -0,0 +1,118 @@
+package com.lynk.game.canyonbunny.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+import com.lynk.game.canyonbunny.util.Constants;
+
+/**
+ * Created by Lynk on 14-6-27.
+ */
+public class Assets implements Disposable, AssetErrorListener {
+    public static final String TAG = Assets.class.getName();
+    public static final Assets instance = new Assets();
+    private AssetManager assetManager;
+
+    public AssetBunny bunny;
+    public AssetRock rock;
+    public AssetGoldCoin goldCoin;
+    public AssetFeather feather;
+    public AssetLevelDecoration levelDecoration;
+
+    private Assets(){
+    }
+
+    public void init(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        //Set asset manager error handler
+        assetManager.setErrorListener(this);
+        //Load texture atlas
+        assetManager.load(Constants.TEXTURE_ATLAS_OBJECT, TextureAtlas.class);
+        //Start loading assets and wait until finished
+        assetManager.finishLoading();
+        Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size);
+        for(String a: assetManager.getAssetNames()) {
+            Gdx.app.debug(TAG, "asset: " + a);
+        }
+
+        TextureAtlas atlas = assetManager.get(Constants.TEXTURE_ATLAS_OBJECT);
+
+        //Enable texture filter for pixel smoothing
+        for(Texture t: atlas.getTextures()) {
+            t.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        }
+
+        //create game resource objects
+        bunny = new AssetBunny(atlas);
+        rock = new AssetRock(atlas);
+        goldCoin = new AssetGoldCoin(atlas);
+        feather = new AssetFeather(atlas);
+        levelDecoration = new AssetLevelDecoration(atlas);
+    }
+
+    @Override
+    public void error(AssetDescriptor asset, Throwable throwable) {
+       Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", throwable);
+    }
+
+    @Override
+    public void dispose() {
+        assetManager.dispose();
+    }
+
+    public class AssetBunny {
+        public final TextureAtlas.AtlasRegion head;
+
+        public AssetBunny(TextureAtlas atlas) {
+            head = atlas.findRegion("bunny_head");
+        }
+    }
+
+    public class AssetRock {
+        public final TextureAtlas.AtlasRegion edge;
+        public final TextureAtlas.AtlasRegion middle;
+
+        public AssetRock(TextureAtlas atlas) {
+            edge = atlas.findRegion("rock_edge");
+            middle = atlas.findRegion("rock_middle");
+        }
+    }
+
+    public class AssetGoldCoin {
+        public final TextureAtlas.AtlasRegion goldCoin;
+
+        public AssetGoldCoin(TextureAtlas atlas) {
+            goldCoin = atlas.findRegion("item_gold_coin");
+        }
+    }
+
+    public class AssetFeather {
+        public final TextureAtlas.AtlasRegion feather;
+
+        public AssetFeather(TextureAtlas atlas) {
+            feather = atlas.findRegion("item_feather");
+        }
+    }
+
+    public class AssetLevelDecoration {
+        public final TextureAtlas.AtlasRegion cloud01;
+        public final TextureAtlas.AtlasRegion cloud02;
+        public final TextureAtlas.AtlasRegion cloud03;
+        public final TextureAtlas.AtlasRegion mountainLeft;
+        public final TextureAtlas.AtlasRegion mountainRight;
+        public final TextureAtlas.AtlasRegion waterOverlay;
+
+        public AssetLevelDecoration(TextureAtlas atlas) {
+            cloud01 = atlas.findRegion("clound01");
+            cloud02 = atlas.findRegion("clound02");
+            cloud03 = atlas.findRegion("clound03");
+            mountainLeft = atlas.findRegion("mountain_left");
+            mountainRight = atlas.findRegion("mountain_right");
+            waterOverlay = atlas.findRegion("water_overlay");
+        }
+    }
+}
